8c99181173e56c16bdf70df5ea8d233d063cb831
TheAlexGo <alex280702@mail.ru>
2022-04-12 22:37:28 +0300

    Добавьте файлы проекта.

---
+++ demoExamen/Pages/LoginPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Controls;
+namespace demoExamen.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для LoginPage.xaml
+    /// </summary>
+    public partial class LoginPage : Page
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+            captcha.CreateCaptcha(EasyCaptcha.Wpf.Captcha.LetterOption.Alphanumeric, 4);
+        }
+    }
+}
